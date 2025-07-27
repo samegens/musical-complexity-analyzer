04ed1b28952f3a77dea73e8024df2a24db4902e0
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-07-27 07:21:21 +0200

    Added: run-coverage.sh.

---
+++ run-coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if ! command -v cargo-llvm-cov &> /dev/null; then
+    cargo install cargo-llvm-cov
+else
+    echo "cargo-llvm-cov already installed"
+fi
+
+COMMON_ARGS="--ignore-filename-regex=main\.rs --all-features --workspace --show-missing-lines"
+
+cargo llvm-cov $COMMON_ARGS \
+  --codecov \
+  --output-path codecov.json \
+  --fail-under-lines 90
+
+cargo llvm-cov $COMMON_ARGS --html
