7640af43660c29dbe925adfe76a2255af9ec8bf1
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2026-02-18 17:26:16 +0100

    Added: data for next improvement.

--- src/crates/musicxml-analysis/src/model/measure_data.rs
+++ src/crates/musicxml-analysis/src/model/measure_data.rs
@@ -10,6 +10,21 @@ pub struct MeasureData {
     pub tempo_bpm: f64,
     pub time_signature: TimeSignature,
     pub pitches: HashSet<Pitch>,
+    pub left_hand_keys: Vec<u8>,  // Upper piano keys for left hand this measure
+    pub right_hand_keys: Vec<u8>, // Upper piano keys for right hand this measure
+}
+
+impl MeasureData {
+    pub fn new(note_count: u32, tempo_bpm: f64, time_signature: TimeSignature, pitches: HashSet<Pitch>) -> Self {
+        Self {
+            note_count,
+            tempo_bpm,
+            time_signature,
+            pitches,
+            left_hand_keys: Vec::new(),
+            right_hand_keys: Vec::new(),
+        }
+    }
 }
 
 impl MeasureData {
@@ -29,12 +44,12 @@ mod tests {
     #[test]
     fn test_get_measure_duration_seconds_standard_4_4_at_120_bpm() {
         // Arrange
-        let measure_data = MeasureData {
-            note_count: 4,
-            tempo_bpm: 120.0,
-            time_signature: TimeSignature::new(4, 4),
-            pitches: HashSet::new(),
-        };
+        let measure_data = MeasureData::new(
+            4,
+            120.0,
+            TimeSignature::new(4, 4),
+            HashSet::new(),
+        );
 
         // Act
         let actual = measure_data.get_measure_duration_seconds();
